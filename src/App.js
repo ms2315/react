b426a27bb19139060d88bac98c58bc81804c074c
ms2315 <sainimohit2315@gmail.com>
2023-07-10 23:55:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import{useState} from 'react';
 import './App.css';
 
 function App() {
+  const[count,setCount] = useState(0);
+
+  function incHandler()
+  {
+    setCount(count+1);
+  }
+  function decHandler()
+  {
+    setCount(count-1);
+  }
+  function resetHandler()
+  {
+    setCount(0);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Heading">Increment & Decrement</div>
+
+      <div className="mid">
+        <button className="button-1" onClick={decHandler}>-</button>
+        <div className="count">{count}</div>
+        <button className="button-2"  onClick={incHandler}>+</button>
+      </div>
+      <button className="reset" onClick={resetHandler}>Reset</button>
     </div>
   );
 }
