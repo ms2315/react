93dffb51fe16c94e064536518af29aa0e2d41aee
ms2315 <sainimohit2315@gmail.com>
2023-07-11 18:06:44 +0530

    refresh update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,16 @@ function App() {
       const newTours = tours.filter(tour => tour.id !== id);
       setTours(newTours);
   }
-  
+  if(tours.length === 0){
+    return(
+      <div className='refresh'>
+        <h2 className='Notoursleft'>No Tours Left</h2>
+        <button className='btn-white' onClick = {()=> setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    )
+  }
   return (
     <div className='App'>
 
